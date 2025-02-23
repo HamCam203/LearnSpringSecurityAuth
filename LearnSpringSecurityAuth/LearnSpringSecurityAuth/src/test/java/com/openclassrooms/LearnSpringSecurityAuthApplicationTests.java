298550a5e091356f1ee91042a7ccf387d1ab9860
HamCam203 <camarahamoudia@gmail.com>
2025-02-23 22:00:48 +0100

    Add authentication tests with MockMvc and JUnit 5

--- LearnSpringSecurityAuth/LearnSpringSecurityAuth/src/test/java/com/openclassrooms/LearnSpringSecurityAuthApplicationTests.java
+++ LearnSpringSecurityAuth/LearnSpringSecurityAuth/src/test/java/com/openclassrooms/LearnSpringSecurityAuthApplicationTests.java
@@ -1,13 +1,23 @@
 package com.openclassrooms;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.openclassrooms.controllers.LoginController;
+
 @SpringBootTest
 class LearnSpringSecurityAuthApplicationTests {
+	
+	@Autowired
+	private LoginController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
